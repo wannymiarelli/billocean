440b89c6a6d68a63b7aed0aebee6648238a0fa5c
Wanny Miarelli <wannymiarelli@live.com>
2016-06-30 17:18:00 +0200

    Fully implemented JWT auth

---
+++ public/app/src/components/Login.vue
@@ -0,0 +1,105 @@
+<style lang="sass">
+  .login-box{
+    width: 30%;
+    text-align: center;
+    margin: 0 auto;
+
+    .login-form{
+      margin-top: 40px;
+    }
+
+  }
+
+  h4{
+    font-size: 1.6rem;
+    font-weight: 500;
+    line-height: 20px;
+    small{
+      color: #0D47A1;
+      font-weight: 300;
+      font-size: 60%;
+    }
+  }
+
+  button{
+    margin-top: 20px;
+  }
+
+  .actions{
+    margin-top: 30px;
+    font-weight: 300;
+  }
+
+  .login-error{
+    color: #C62828;
+    margin-top: 50px;
+  }
+</style>
+<template>
+  <div class="valign-wrapper">
+    <div class="valign">
+
+      <div class="login-box">
+        <div class="login-title">
+          <h4>
+            BILLOCEAN <br>
+            <small>here to simplify your business</small>
+          </h4>
+        </div>
+
+        <div class="login-error" v-show="loginError">
+          <i class="material-icons">error</i> Credenziali non valide, riprova.
+        </div>
+
+        <div class="login-form">
+
+          <div class="input-wrapper">
+            <input type="email" name="email" v-model="user.email" placeholder="Email">
+          </div>
+
+          <div class="input-wrapper">
+            <input type="password" name="password" v-model="user.password" placeholder="Password">
+          </div>
+
+          <button type="submit" :disabled="loginAttempt" v-on:click="login" class="btn btn-primary btn-extended">Accedi</button>
+
+        </div>
+
+        <div class="actions">
+          <a href="#"><i class="material-icons">help_outline</i> Ho smarrito la mia password.</a> <br>
+          <a href="#">Sono un nuovo utente.</a>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</template>
+<script type="text/javascript">
+  export default {
+    data() {
+      return {
+        loginError: false,
+        loginAttempt: false,
+        user: {
+          email: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      login() {
+        this.loginAttempt = true;
+        this.$http.post('/api/v1/auth/attempt', this.user).then(function(res){
+          console.log(res);
+          localStorage.setItem('token', res.data.data);
+          this.loginError = false;
+          this.loginAttempt = false;
+        }, function(err){
+          this.loginAttempt = false;
+          this.loginError = true;
+        })
+      }
+    }
+  }
+</script>
--- public/app/src/main.js
+++ public/app/src/main.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource';
 //Import components
-import Hello from './components/Hello.vue'
+import Login from './components/Login.vue'
 //Router configuration
 Vue.use(VueRouter);
+Vue.use(VueResource);
 var router = new VueRouter();
 //Init the root component
 var App = Vue.extend({});
 
 router.map({
   '/': {
-    component: Hello
+    component: Login
   }
 });
 
