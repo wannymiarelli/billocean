8438344a77f0bfe5d423e858abcc563688bb917f
Wanny Miarelli <wannymiarelli@live.com>
2016-06-30 14:42:01 +0200

    Catch all auth errors

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -9,10 +9,19 @@ module.exports = function (app) {
 };
 
 router.post('/register', function(req, res, next){
-  console.log(req.body);
+  //genereate the salt to hash the password
+  bcrypt.genSalt(10, function(err, salt){
+    //hash the password with the generated salt
+    bcrypt.hash(req.body.password, salt, function(err, hash){
+      req.body.password = hash;
+    });
+  });
+  //Attempt to save the user in the database
   db.User.create(req.body).then(function(user){
+    //Successfuly created, stored and sent back to the client.
     res.json(response.success(user));
   }).catch(function(error){
+    //Error occured, send back the info to the client.
     res.json(response.error('500', error.message))
   });
 });
@@ -33,6 +42,9 @@ router.get('/attempt', function (req, res, next) {
         //user found so we try to compare the password
 
       }
+    }).catch(function(error){
+      //error occured during login attemp
+      res.json(response.error(500, error.message));
     });
   }
 });
